efb7dc80f19c23df96d9f0472bb9b24f232cebde
JStephens <40901504+JStephens@users.noreply.github.com>
2021-09-20 20:50:42 -0700

    Commit/push to continue work on another machine :)

--- leads/models.py
+++ leads/models.py
@@ -9,18 +9,3 @@ class Lead(models.Model):
     cellphone = models.CharField(max_length=10)
     propertiesShown = models.PositiveIntegerField(default=0)
 
-
-class Broker(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    licensee_name = models.CharField(max_length=200)
-
-
-class Agent(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-
-
-class BrokerAgent(models.Model):
-    broker_id = models.ForeignKey(Broker, on_delete=models.CASCADE)
-    agent_id = models.ForeignKey(Agent, on_delete=models.CASCADE)
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,21 @@
+from _typeshed import Self
+from typing_extensions import Required
+from django.core.exceptions import RequestDataTooBig
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_broker = models.BooleanField(default=False)
+    is_agent = models.BooleanField(default=True)
+    broker_name = models.CharField(max_length=200, Required=True)
+    broker_address = models.CharField(max_length=200, Required=True)
+    broker_license_id = models.CharField(max_length=20, Required=True)
+    
+class BrokerAgent(models.Model):
+    broker_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    agent_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
